fcefecf64321176881c584bb3a4c0f5792e7ee2d
Aldo Praherda <aldo@aldoapp.com>
2023-06-19 10:46:21 +0700

    update

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const cors = require('cors')
+app.use(cors());
+
+app.get('/info', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
