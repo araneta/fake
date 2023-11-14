37afc96ce088728791170e0cd68d328cb5c4a79b
aldo <aldo@aldoapp.com>
2023-11-15 05:41:01 +0700

    updated

--- src/utils/supabase.ts
+++ src/utils/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 import 'dotenv/config';
-console.log('url',process.env.NEXT_PUBLIC_SUPABASE_URL);
+console.log('urkkl',process.env.NEXT_PUBLIC_SUPABASE_URL);
 export default createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL,
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
